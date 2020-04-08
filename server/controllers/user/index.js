e9875141d012519297c78f10ba892a019334ca55
James Hawkins <jhawki2005@gmail.com>
2020-04-08 09:42:50 +0100

    Password hashing!

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -1,1 +1,8 @@
 const db = require('../../helpers/db');
+const crypto = require('crypto');
+
+function createUser(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const passwordHash = crypto.createHash('md5').update(password).digest('hex');
+}
