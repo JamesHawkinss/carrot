9692bf070386c4dae8869b2ba67895a12a330931
weeryan17 <theweeryan17@gmail.com>
2020-04-19 11:00:44 -0500

    Token generation

--- server/controllers/user/auth/index.js
+++ server/controllers/user/auth/index.js
@@ -1,27 +1,69 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const db = require('../../../helpers/db');
 
 function auth(req, res) {
     if (!req.body.username || !req.body.password) {
-        return res.status(406).send({ "result": "expected username and password" });
+        return res.status(406).send({"result": "expected username and password"});
     }
     const username = req.body.username;
     const password = req.body.password;
-    
+
     db.query({
-        sql: 'SELECT passwordHash FROM users WHERE username = (?)',
+        sql: 'SELECT id, passwordHash FROM users WHERE username = (?)',
         values: [username]
     }, function (err, results) {
-        if (err) return res.status(500).send({ "result": "database error" });
-        bcrypt.compare(password, results.passwordHash, function(err, result) {
-            if (err) return res.status(500).send({ "result": "database error" });
+        if (err) return res.status(500).send({
+            "result": "ERROR",
+            "error": {
+                "cause": "DATABASE",
+                "trace": err
+            }
+        });
+        var id = results.id;
+        bcrypt.compare(password, results.passwordHash, function (err, result) {
+            if (err) return res.status(500).send({
+                "result": "ERROR",
+                "error": {
+                    "cause": "CRYPT",
+                    "trace": err
+                }
+            });
             if (result) {
-                res.send({ "result": "auth OK" });
+                crypto.randomBytes(128, function (err, buf) {
+                    if (err) return res.status(500).send({
+                        "result": "ERROR",
+                        "error": {
+                            "cause": "CRYPT",
+                            "trace": err
+                        }
+                    });
+                    var token = buf.toString('hex');
+                    db.query({
+                        sql: 'UPDATE users SET token = ? WHERE id = ?',
+                        values: [token, id]
+                    }, function (err, results) {
+                        if (err) return res.status(500).send({
+                            "result": "ERROR",
+                            "error": {
+                                "cause": "DATABASE",
+                                "trace": err
+                            }
+                        });
+                        res.send({
+                            "result": "OK",
+                            "auth": {
+                                "id": id,
+                                "token": token
+                            }
+                        });
+                    });
+                });
             } else {
-                res.send({ "result": "auth FAILED" });
+                res.status(403).send({"result": "FAILED"});
             }
         });
     });
 }
 
-module.exports = { auth }
+module.exports = {auth}
--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -1,4 +1,5 @@
 const db = require('../../helpers/db');
+const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 
 function createUser(req, res) {
@@ -9,19 +10,41 @@ function createUser(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    bcrypt.hash(password, 10, function(err, hash) {
-        if (err) throw err;
-        db.query({
-            sql: 'INSERT INTO users (username, passwordHash) VALUES (?, ?)',
-            values: [username, hash]
-        }, function (err, results) {
-            if (err) return res.status(500).send({ "result": "database error", err });
+    bcrypt.hash(password, 10, function (err, hash) {
+        if (err) return res.status(500).send({
+            "result": "ERROR",
+            "error": {
+                "cause": "CRYPT",
+                "trace": err
+            }
+        });
+        crypto.randomBytes(128, function (err, buf) {
+            if (err) return res.status(500).send({
+                "result": "ERROR",
+                "error": {
+                    "cause": "CRYPT",
+                    "trace": err
+                }
+            });
+            var token = buf.toString('hex');
             db.query({
-                sql: 'SELECT * FROM USERS WHERE username = (?)',
-                values: [username]
+                sql: 'INSERT INTO users (username, passwordHash, token) VALUES (?, ?, ?)',
+                values: [username, hash, token]
             }, function (err, results) {
-                if (err) return res.status(500).send({ "result": "database error", err });
-                return res.send({ "result": "success", results });
+                if (err) return res.status(500).send({
+                    "result": "ERROR",
+                    "error": {
+                        "cause": "DATABASE",
+                        "trace": err
+                    }
+                });
+                return res.send({
+                    "result": "OK",
+                    "auth": {
+                        "id": results.insertId,
+                        "token": token
+                    }
+                });
             });
         });
     });
@@ -29,14 +52,14 @@ function createUser(req, res) {
 
 function getUsers(req, res) {
     db.query('SELECT * FROM users', function (err, results) {
-        if (err) return res.status(500).send({ "result": "database error", err });
+        if (err) return res.status(500).send({"result": "database error", err});
         res.send(results);
     });
 }
 
 function getUser(req, res) {
     if (!req.params.id || isNaN(req.params.id)) {
-        return res.status(406).send({ "result": "expected numeric id" });
+        return res.status(406).send({"result": "expected numeric id"});
     }
     const id = req.params.id;
     db.query({
@@ -46,14 +69,14 @@ function getUser(req, res) {
         if (err || results.length <= 0) {
             res.sendStatus(404);
         } else {
-            return res.send({ "result": "success", results });
+            return res.send({"result": "success", results});
         }
     });
 }
 
 function deleteUser(req, res) {
     if (!id || NaN(id)) {
-        return res.status(406).send({ "result": "expected numeric id" });
+        return res.status(406).send({"result": "expected numeric id"});
     }
     const id = req.params.id;
     db.query({
